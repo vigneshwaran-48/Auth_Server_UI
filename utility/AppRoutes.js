f88241dfb68489aaf4c879b59dce835004dcbc1c
vigneshwaran48 <p3487260@gmail.com>
2024-05-05 16:34:24 +0530

    Added routes for developer console

--- utility/AppRoutes.js
+++ utility/AppRoutes.js
@@ -5,10 +5,22 @@ export const RoutesManager = {
     home: "/"
 }
 
+let appRoutes = {};
+
 export const ServerAPIManager = {
     
     resourceServerBase: `http://${window.location.hostname}:9393`,
     userCreateAPI: "/api/user",
     userInfoAPI: "/api/user/me",
-    userLoginAPI: "/authenticate"
+    userLoginAPI: "/authenticate",
+
+    getAppRoutes: async () => {
+        if(!appRoutes || !Object.keys(appRoutes).length) {
+
+            const response = await fetch("/api/utility/get-routes");
+            const data = await response.json();
+            appRoutes = data;
+        }
+        return appRoutes;
+    }
 }
